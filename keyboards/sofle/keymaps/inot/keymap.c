c1e1db7731771f2a73d992bc8fd873c065a81f75
Anton Nosov <inot@live.ru>
2023-05-19 21:23:11 +0300

    keymap

--- keyboards/sofle/keymaps/inot/keymap.c
+++ keyboards/sofle/keymaps/inot/keymap.c
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_FPS] = LAYOUT(KC_TRNS             , KC_1                , KC_2                , KC_3                , KC_4                , KC_5                , KC_6                , KC_7                , KC_8                , KC_9                , KC_0                , KC_BSPACE           , 
              KC_TAB              , KC_Q                , KC_W                , KC_E                , KC_R                , KC_T                , KC_Y                , KC_U                , KC_I                , KC_O                , KC_P                , KC_LBRACKET         , KC_LSHIFT           , 
-             A_D                 , KC_S                , D_A                 , KC_F                , KC_G                , KC_H                , KC_J                , KC_K                , KC_L                , KC_SCOLON           , KC_QUOTE            , KC_LSHIFT           , 
+             KC_A                , KC_S                , KC_D                , KC_F                , KC_G                , KC_H                , KC_J                , KC_K                , KC_L                , KC_SCOLON           , KC_QUOTE            , KC_LSHIFT           , 
              KC_Z                , KC_X                , KC_C                , KC_V                , KC_B                , KC_TRNS             , KC_TRNS             , KC_N                , KC_M                , KC_COMMA            , KC_DOT              , KC_SLASH            , KC_TRNS             , 
              KC_NO               , KC_NO               , JUMP_SIT            , KC_LCTRL            , KC_TRNS             , KC_TRNS             , KC_TRNS             , KC_NO               , KC_NO               , KC_NO               ),
 
@@ -107,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [_DEFAULT] = {  ENCODER_CCW_CW(KC_VOLD, KC_VOLU),           ENCODER_CCW_CW(KC_LEFT, KC_RIGHT)},
     [_FPS] =     {  ENCODER_CCW_CW(KC_TRNS, KC_TRNS),           ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
-    [_LOWER] =   {  ENCODER_CCW_CW(KC_TRNS, KC_TRNS),           ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
+    [_LOWER] =   {  ENCODER_CCW_CW(KC_TRNS, KC_TRNS),           ENCODER_CCW_CW(KC_UP, KC_DOWN)  },
     [_RAISE] =   {  ENCODER_CCW_CW(KC_TRNS, KC_TRNS),           ENCODER_CCW_CW(DF(1), DF(4)) },
     [_WOW] =     {  ENCODER_CCW_CW(KC_TRNS, KC_TRNS),           ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
 };
